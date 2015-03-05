aad57cc80f38e71055f1b81667d401ab24909c2a
andreacirilloac <andreacirilloac@gmail.com>
2015-03-05 22:20:46 +0100

    adding description to control chart_script

---
+++ control_script.R
@@ -0,0 +1,9 @@
+# calculate the central limit for the numeric variable
+# calculate upper limit
+#calculat the lower limit
+# function
+  # if the number is within the range
+  # score is 0
+  #esle
+  # score is the difference
+# remove all columns except score
