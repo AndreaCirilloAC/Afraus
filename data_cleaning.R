dc1b2742be312e25c5c24cd294a8e5a2473f822e
andreacirilloac <andreacirilloac@gmail.com>
2015-03-06 15:34:48 +0100

    first cleaning and synthetic data loaded

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,9 +1,27 @@
+
 # data cleaning script, taking user-data.frame and giving a data.frame usable for afraus score
-# read file
+# read file, CSV only
+path  <-  "data.csv"
+data <- read.csv(path,sep = ";", header = TRUE)
 # check file properties raising appropriate exception
   #two columns?
-  #dates on first column?
-  #numbers on second column?
+if(colnums(data)!=2){
+  stop(paste("the loaded dataset has ",ncol," columns. Please load a two columns dataset" )
+}
+    #dates on first column?
+if(is.number(data[,2])=FALSE|is.date(data[,2])=FALSE|){
+  stop(paste("the loaded dataset doestn't contain numbers on the second column. 
+Please provide a dataset with numbers on that column"))
+}
+    #numbers on second column?
+if(is.number(data[,2])=FALSE){
+  stop(paste("the loaded dataset doestn't contain numbers on the second column. 
+Please provide a dataset with numbers on that column"))
+}
+#do some wrangling  
+  # sort descending
 
-#do some wrangling
-  #add a new column converting date in number, assuming a mm/dd/yyy format
+e <- simpleError("test error")
+## Not run: 
+stop(e)
+tryCatch(stop(e), finally = print("Hello"))
