75a65bf3bcc1587e0f1a8dd90babad733f84eeca
andreacirilloac <andreacirilloac@gmail.com>
2015-03-06 21:54:13 +0100

    control_script stable version, substituted apply with sapply.

--- control_script.R
+++ control_script.R
@@ -9,15 +9,17 @@ moving_range  <- sum(ranges)/(ncol(data)-1)
 UCL  <-   central_limit +3*(moving_range/1.128)
 #calculate the lower limit
 LCL  <-   central_limit -3*(moving_range/1.128)
-control_scoring   <- function(n,upper_control_limit,lower_control_limit,central_limit){}
-n <- data[5,2]
+# function
+# if the number is within the range
+# score is 0
+#esle
+# score is the difference
+control_scoring   <- function(n,upper_control_limit,lower_control_limit,central_limit){
 if (n>UCL|n<LCL){
   dist <- abs(n-central_limit)
 }else{0}
+}
+# apply function to dataset values
+data$control_score <- sapply(data[,2],FUN=control_scoring,upper_control_limit=UCL,
+                            lower_control_limit= LCL,central_limit=central_limit)
 
-# function
-  # if the number is within the range
-  # score is 0
-  #esle
-  # score is the difference
-# remove all columns except score
--- main.R
+++ main.R
@@ -6,6 +6,8 @@ library(assertthat)
 source("data_cleaning.R")
 #run benford_script
 source("benford_script.R")
+#run control_script
+
 #run lof_script
 source("lof_script.R")
 #run afraus_score
