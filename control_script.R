84feb1d3f901074242633d4fe0f11c0a4f263705
andreacirilloac <andreacirilloac@gmail.com>
2015-03-06 20:49:13 +0100

    control chart script first version, score function not working

--- control_script.R
+++ control_script.R
@@ -1,6 +1,20 @@
-# calculate the central limit for the numeric variable
-# calculate upper limit
-#calculat the lower limit
+# calculate the central limit for the numeric variable as the records average
+central_limit <- mean(data[,2])
+
+n_1           <- data[-1,2]
+n             <- data[-nrow(data),2]
+ranges        <- n_1-n
+moving_range  <- sum(ranges)/(ncol(data)-1) 
+  # calculate upper limit
+UCL  <-   central_limit +3*(moving_range/1.128)
+#calculate the lower limit
+LCL  <-   central_limit -3*(moving_range/1.128)
+control_scoring   <- function(n,upper_control_limit,lower_control_limit,central_limit){}
+n <- data[5,2]
+if (n>UCL|n<LCL){
+  dist <- abs(n-central_limit)
+}else{0}
+
 # function
   # if the number is within the range
   # score is 0
