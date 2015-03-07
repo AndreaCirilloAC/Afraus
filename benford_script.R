46b938d6444d57c7c77120f457b951453c8af39d
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-03-07 18:07:07 +0100

    benford_script modified: introduced z-score and pvalue in order to evaluate deviation significance for each record

--- benford_script.R
+++ benford_script.R
@@ -11,19 +11,27 @@ distr_digits <- as.numeric(left(abs(data[,2]),1))
 #next
 
 # compute table of frequencies
-percent_distr           <- data.frame(table(distr_digits)/length(distr_digits)*100)
+percent_distr           <- data.frame(table(distr_digits)/length(distr_digits))
 colnames(percent_distr) <- c("digit","real_freq")
 percent_distr[,1]       <- as.numeric(as.character(percent_distr[,1])) 
 percent_distr[,2]       <- round(percent_distr[,2],4)
 # compute the ideal table ofr frequencies
 digit      <- seq(1:9)
-benf_freq  <- round((log(1+(1/ digit),10))*100,4)
-benf_distr <- data.frame(digit,benf_freq)
+benf_freq  <- round((log(1+(1/ digit),10)),4)
+
+#based on Nigrini (1996), calculate a z-statistic in order to assess the statistical 
+# significance of deviations
+
+# first compute standard deviation
+
+dev        <- (((benf_freq))*(1-((benf_freq)))/nrow(data))^(1/2)
+benf_distr <- data.frame(digit,benf_freq,dev)
 test_table <- merge(benf_distr,percent_distr, by="digit",all.x=TRUE) 
 
+# compute z-statistic
+test_table$z <- (abs(test_table$real_freq-test_table$benf_freq)-1/(2*nrow(data)))/dev
+test_table$pvalue <- pnorm(-abs(z))
 
-# for each number calculate the difference between the real and ideal distribution
-test_table$difference <- test_table$benf_freq - test_table$real_freq
 # match records with difference
 
 data$digit        <- as.numeric(left(abs(data[,2]),1))
@@ -34,3 +42,4 @@ data              <- merge( data,test_table, by= "digit",all.x=TRUE)
 
 data           <- data.frame(data[,2:3],data[,6]) 
 colnames(data) <- c("date","value","benford_difference(%)") 
+}
