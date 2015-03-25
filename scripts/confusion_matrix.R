65aa71ee96aa94f8e3012ef4c1a76cd2348d2fcf
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-03-25 06:24:03 +0100

    introduction of confusion matrix derivations

--- scripts/confusion_matrix.R
+++ scripts/confusion_matrix.R
@@ -1,2 +1,12 @@
-results <- data.matrix(table(data$test,data$score_filter))
-sum(diag(results))
+data$afraus_score <- data$benford_score*1+data$control_score*0+data$lof_score*0
+data$score_filter <- data$afraus_score>0.5
+results           <- data.matrix(table(data$test,data$score_filter))
+
+TP        <- results[2,2]
+TN        <- results[1,1]
+Accuracy  <- (TP+TN)/(nrow(data))
+FP        <- results[1,2]
+FN        <- results[2,1]
+precision <- TP/(TP+FP)
+F1        <- (2*TP)/(2*TP+FP+FN)
+
