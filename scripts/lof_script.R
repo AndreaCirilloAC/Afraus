b915c2978574bad17052bf3d262460dce4292bde
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-10 06:07:44 +0200

    removed script from first level. All updated script into Afraus_app

--- scripts/lof_script.R
+++
@@ -1,9 +0,0 @@
-
-# calculate lof for the quantitative variable
-data$lof <- lofactor(data[,2],k=2)
-
-# for lof > 1 outlier
-# for lof < 1 no outlier
-# assign score depending on lof
-# assign 0 if no outlier
-
